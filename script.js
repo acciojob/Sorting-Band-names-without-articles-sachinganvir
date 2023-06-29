6ca749af559443a13ff8c68035f1aef9f0e69af6
sachinganvir <120235946+sachinganvir@users.noreply.github.com>
2023-06-29 19:22:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 //your code here
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+touristSpots.sort((a, b) => {
+    const articles = /^(The|A|An)\s+/i; 
+
+    const spotA = a.replace(articles, '');
+    const spotB = b.replace(articles, '');
+  
+    return spotA.localeCompare(spotB);
+  });
+console.log(touristSpots);
+  
+ const ulElement = document.getElementById('band');
+
+
+ulElement.innerHTML = '';
+
+
+touristSpots.forEach((spot) => {
+  const liElement = document.createElement('li');
+  liElement.textContent = spot;
+  ulElement.appendChild(liElement);
+});
 
