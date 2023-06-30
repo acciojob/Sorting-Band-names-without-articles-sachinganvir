07d5a34ce948e0c54653b15a8563b50cfb5e2d0c
sachinganvir <120235946+sachinganvir@users.noreply.github.com>
2023-06-30 16:35:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,20 @@
-//your code here
-let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
-touristSpots.sort((a, b) => {
-    const articles = /^(The|A|An)\s+/i; 
-
-    const spotA = a.replace(articles, '');
-    const spotB = b.replace(articles, '');
-  
-    return spotA.localeCompare(spotB);
-  });
-console.log(touristSpots);
-  
- const ulElement = document.getElementById('band');
-
-
-ulElement.innerHTML = '';
-
-
-touristSpots.forEach((spot) => {
-  const liElement = document.createElement('li');
-  liElement.textContent = spot;
-  ulElement.appendChild(liElement);
-});
-
+let bandNames=['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal']
+let articles = ['A', 'AN', 'THE']
+ 
+	bandNames.sort((a,b) => (strip(a)> strip(b))? 1: -1)
+	const ref= document.getElementsByTagName("ul")[0] 
+	for (let i=0;i<bandNames.length; i++){
+		const li = document.createElement("li")
+		li.innerText= bandNames [i]
+		ref.append(li)
+	}
+function strip(word){
+	let arr = word.split(" ")
+	let s=""
+	for(let i=0;i<arr.length;i++){
+if(articles.indexOf(arr[i].toUpperCase()) === -1){
+	s=s+arr[i]
+}
+}
+return s.trim()
+}
